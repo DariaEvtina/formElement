28604987f7c24f257c1b0db59cf24e55d5b46e5a
Ervin_liiv <opilane@HP-CZC6297B8P>
2021-11-24 15:26:13 +0200

    hehe....

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace formElements
 {
@@ -16,10 +17,17 @@ namespace formElements
         Button btn;
         Label lbl;
         PictureBox pbox;
-        public CheckBox ckbox;
+        CheckBox ckbox;
         CheckBox ckbox1;
         CheckBox ckbox2;
         CheckBox ckbox3;
+        RadioButton rb;
+        RadioButton rb1;
+        RadioButton rb2;
+        TextBox txt;
+
+
+
         public Form1()
         {
             this.Height = 500;
@@ -48,7 +56,7 @@ namespace formElements
             //label
             lbl = new Label();
             lbl.Text = "Elementide loomine c# abil";
-            lbl.Size = new Size(140,15);
+            lbl.Size = new Size(lbl.Text.Length*8,15);
             lbl.Location = new Point(150, 5);
             lbl.MouseHover += Lbl_MouseHover;
             lbl.MouseLeave += Lbl_MouseLeave;
@@ -62,18 +70,19 @@ namespace formElements
             pbox.SizeMode = PictureBoxSizeMode.StretchImage;
             pbox.MouseDoubleClick += Pbox_MouseDoubleClick;
             pbox.MouseLeave += Pbox_MouseLeave;
-            ///
+            pbox.BorderStyle = BorderStyle.FixedSingle;
+            ///checkbox
             ckbox = new CheckBox();
             ckbox.Size = new Size(90, 30);
             ckbox.Text = "Background";
-            ckbox.Location = new Point(320, 10);
+            ckbox.Location = new Point(320, 19);
             ckbox.CheckedChanged += Ckbox_CheckedChanged;
             ckbox.Checked = new bool();
             ///
             ckbox1 = new CheckBox();
             ckbox1.Size = new Size(90, 30);
             ckbox1.Text = "Font";
-            ckbox1.Location = new Point(320, 40);
+            ckbox1.Location = new Point(320, 49);
             ckbox1.Checked = new bool();
             ckbox1.CheckedChanged += Ckbox1_CheckedChanged;
             ///
@@ -81,16 +90,166 @@ namespace formElements
             ckbox2.Size = new Size(90, 30);
             ckbox2.Text = "Size";
             ckbox2.CheckedChanged += Ckbox2_CheckedChanged;
-            ckbox2.Location = new Point(320, 70);
+            ckbox2.Location = new Point(320, 79);
             ckbox2.Checked = new bool();
             ///
             ckbox3 = new CheckBox();
             ckbox3.Size = new Size(90, 90);
-            ckbox3.Location = new Point(320, 100);
+            ckbox3.Location = new Point(320, 109);
             ckbox3.Text = "cat";
             ckbox3.Checked = new bool();
             ckbox3.CheckedChanged += Ckbox3_CheckedChanged;
+            //radiobutton
+            rb = new RadioButton();
+            rb.Size = new Size(90,30);
+            rb.Location = new Point(150,400);
+            rb.Text = "red";
+            rb.Checked = new bool();
+            rb.CheckedChanged += Rb_CheckedChanged;
+            rb1 = new RadioButton();
+            rb1.Size = new Size(90, 30);
+            rb1.Location = new Point(150, 430);
+            rb1.Text = "blue";
+            rb1.Checked = new bool();
+            rb1.CheckedChanged += Rb1_CheckedChanged;
+            rb2 = new RadioButton();
+            rb2.Size = new Size(90, 30);
+            rb2.Location = new Point(150, 370);
+            rb2.Text = "Regular";
+            rb2.Checked = new bool();
+            rb2.CheckedChanged += Rb2_CheckedChanged;
+        }
 
+        private void Rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(120, 10, 10);
+                lbl.ForeColor = Color.FromArgb(130, 130, 100);
+                btn.ForeColor = Color.FromArgb(130, 130, 100);
+                btn.BackColor = Color.FromArgb(150, 10, 100);
+                btn.Text = "REED!!";
+                ckbox.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox1.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox2.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox3.ForeColor = Color.FromArgb(130, 130, 100);
+                pbox.Image = Image.FromFile(@"../../img/RED.png");
+            }
+            else if (rb1.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(50, 20, 250);
+                lbl.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.BackColor = Color.FromArgb(20, 200, 250);
+                btn.Text = "BLUUES!!";
+                ckbox.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox1.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox2.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox3.ForeColor = Color.FromArgb(200, 200, 250);
+                pbox.Image = Image.FromFile(@"../../img/blue.png");
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(250, 250, 250);
+                lbl.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.BackColor = Color.Transparent;
+                btn.Text = "click";
+                ckbox.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox1.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox2.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox3.ForeColor = Color.FromArgb(0, 0, 0);
+                pbox.Image = Image.FromFile(@"../../img/cat.png");
+
+            }
+        }
+
+        private void Rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(120, 10, 10);
+                lbl.ForeColor = Color.FromArgb(130, 130, 100);
+                btn.ForeColor = Color.FromArgb(130, 130, 100);
+                btn.BackColor = Color.FromArgb(150, 10, 100);
+                btn.Text = "REED!!";
+                ckbox.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox1.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox2.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox3.ForeColor = Color.FromArgb(130, 130, 100);
+                pbox.Image = Image.FromFile(@"../../img/RED.png");
+            }
+            else if (rb1.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(50, 20, 250);
+                lbl.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.BackColor = Color.FromArgb(20, 200, 250);
+                btn.Text = "BLUUES!!";
+                ckbox.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox1.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox2.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox3.ForeColor = Color.FromArgb(200, 200, 250);
+                pbox.Image = Image.FromFile(@"../../img/blue.png");
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(250, 250, 250);
+                lbl.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.BackColor = Color.Transparent;
+                btn.Text = "click";
+                ckbox.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox1.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox2.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox3.ForeColor = Color.FromArgb(0, 0, 0);
+                pbox.Image = Image.FromFile(@"../../img/cat.png");
+
+            }
+        }
+
+        private void Rb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(120, 10, 10);
+                lbl.ForeColor = Color.FromArgb(130,130,100);
+                btn.ForeColor = Color.FromArgb(130, 130, 100);
+                btn.BackColor= Color.FromArgb(150, 10, 100);
+                btn.Text = "REED!!";
+                ckbox.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox1.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox2.ForeColor = Color.FromArgb(130, 130, 100);
+                ckbox3.ForeColor = Color.FromArgb(130, 130, 100);
+                pbox.Image = Image.FromFile(@"../../img/RED.png");
+            }
+            else if (rb1.Checked == true)
+            {
+                this.BackColor = Color.FromArgb(50, 20, 250);
+                lbl.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.ForeColor = Color.FromArgb(200, 200, 250);
+                btn.BackColor = Color.FromArgb(20, 200, 250);
+                btn.Text = "BLUEEES!!";
+                ckbox.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox1.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox2.ForeColor = Color.FromArgb(200, 200, 250);
+                ckbox3.ForeColor = Color.FromArgb(200, 200, 250);
+                pbox.Image = Image.FromFile(@"../../img/blue.png");
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(250, 250, 250);
+                lbl.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.ForeColor = Color.FromArgb(0, 0, 0);
+                btn.BackColor = Color.Transparent;
+                btn.Text = "click";
+                ckbox.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox1.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox2.ForeColor = Color.FromArgb(0, 0, 0);
+                ckbox3.ForeColor = Color.FromArgb(0, 0, 0);
+                pbox.Image = Image.FromFile(@"../../img/cat.png");
+
+            }
         }
 
         private void Btn_Leave(object sender, EventArgs e)
@@ -188,11 +347,11 @@ namespace formElements
 
         private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            if (e.Node.Text=="button")
+            if (e.Node.Text == "button")
             {
                 this.Controls.Add(btn);
             }
-            else if (e.Node.Text=="label")
+            else if (e.Node.Text == "label")
             {
                 this.Controls.Add(lbl);
             }
@@ -206,6 +365,35 @@ namespace formElements
                 this.Controls.Add(ckbox1);
                 this.Controls.Add(ckbox2);
                 this.Controls.Add(ckbox3);
+            }
+            else if (e.Node.Text == "radiobutton")
+            {
+                this.Controls.Add(rb);
+                this.Controls.Add(rb1);
+                this.Controls.Add(rb2);
+            }
+            else if (e.Node.Text == "messagebox")
+            {
+                MessageBox.Show("Messagebox",":3");
+                var answer=MessageBox.Show("Tahab inputbox?","?????",MessageBoxButtons.YesNo);
+                if (answer==DialogResult.Yes)
+                {
+                    string text = Interaction.InputBox("Sisetaja siia mingi tekst", "InputBox", "Mingi text");
+                    if (MessageBox.Show("Kas sa tahab saada tektskastisse?","Teksti salvatamine",MessageBoxButtons.OKCancel)==DialogResult.OK)
+                    {
+                        lbl.Text = text;
+                        Controls.Add(lbl);
+                    }
+                    else
+                    {
+                        MessageBox.Show("fine...", ">:(");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("there will be no turning back, bb LOSER", ">:|");
+                }
+
             }
         }
     }
